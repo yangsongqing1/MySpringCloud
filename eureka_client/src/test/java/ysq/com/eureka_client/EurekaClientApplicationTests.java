41716dc7b149761b38b239d213e93984402a138b
yeyang <yangsq@juzix.io>
2017-11-20 16:28:09 +0800

    服务注册与发现

---
+++ eureka_client/src/test/java/ysq/com/eureka_client/EurekaClientApplicationTests.java
@@ -0,0 +1,17 @@
+package ysq.com.eureka_client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
