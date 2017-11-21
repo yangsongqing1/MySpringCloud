23e8919992d556de227fde61559d9e41e3c6c728
yeyang <yangsq@juzix.io>
2017-11-21 17:18:28 +0800

    zull

--- service-zuul/src/main/java/ysq/com/servicezuul/ServiceZuulApplication.java
+++ service-zuul/src/main/java/ysq/com/servicezuul/ServiceZuulApplication.java
@@ -2,11 +2,19 @@ package ysq.com.servicezuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * 路由转发与过滤类似niginx
+ * TODO断路器不可用
+ */
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceZuulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
 }
