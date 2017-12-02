1cf9055987a4677478bdff97f5f9863f49d51ef3
yeyang <yangsq@juzix.io>
2017-12-02 14:14:47 +0800

    链路追踪

--- server-zipkin/src/main/java/ysq/com/zipkin/ServerZipkinApplication.java
+++ server-zipkin/src/main/java/ysq/com/zipkin/ServerZipkinApplication.java
@@ -2,10 +2,15 @@ package ysq.com.zipkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.EnableZipkinServer;
 
+/**
+ * http://www.cnblogs.com/shunyang/p/7011283.html
+ */
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class ServerZipkinApplication {
 
     public static void main(String[] args) {
